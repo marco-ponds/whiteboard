1e2342b5b14859c7031424650a10bc9c7a290c44
Marco Stagni <m.stagni@elsevier.com>
2019-06-03 17:40:46 +0100

    testing

--- server.js
+++ server.js
@@ -6,21 +6,28 @@ const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const bodyParser = require('body-parser');
 
-const ServerController= require('./serverController');
-const SocketController = require('./SocketController');
+// const ServerController= require('./serverController');
+// const SocketController = require('./SocketController');
 
-server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-server.use(bodyParser.json({ limit: '100mb', extended: true }));
-server.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }));
-server.use(morgan('dev'));
-server.use(express.static('public'));
+// server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+// server.use(bodyParser.json({ limit: '100mb', extended: true }));
+// server.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }));
+// server.use(morgan('dev'));
+// server.use(express.static('public'));
 
-server.post('/api/image', ServerController.uploadImage);
-server.get('/api/image', ServerController.getSingleImage);
-server.get("/", ServerController.sendIndexHtml);
+// server.post('/api/image', ServerController.uploadImage);
+// server.get('/api/image', ServerController.getSingleImage);
+// server.get("/", ServerController.sendIndexHtml);
 
-SocketController.start(io);
-const PORT = process.env.PORt || 5000;
+server.get('/', function(req, res) {
+  res.status(200)
+      .json({ message: 'hello world' });
+});
+// SocketController.start(io);
+
+console.log(process.env);
+
+const PORT = process.env.PORT || 5000;
 
 http.listen(PORT, function(){
   console.log('listening on *:' + PORT);
