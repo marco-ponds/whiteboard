bf98fd72d417513766e294124ead35a373917f22
Marco Stagni <m.stagni@elsevier.com>
2019-06-03 17:46:53 +0100

    step2

--- server.js
+++ server.js
@@ -9,20 +9,16 @@ const bodyParser = require('body-parser');
 const ServerController= require('./serverController');
 const SocketController = require('./SocketController');
 
-// server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-// server.use(bodyParser.json({ limit: '100mb', extended: true }));
-// server.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }));
-// server.use(morgan('dev'));
-// server.use(express.static('public'));
+server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+server.use(bodyParser.json({ limit: '100mb', extended: true }));
+server.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }));
+server.use(morgan('dev'));
+server.use(express.static('public'));
 
-// server.post('/api/image', ServerController.uploadImage);
-// server.get('/api/image', ServerController.getSingleImage);
+server.post('/api/image', ServerController.uploadImage);
+server.get('/api/image', ServerController.getSingleImage);
 server.get("/", ServerController.sendIndexHtml);
 
-server.get('/', function(req, res) {
-  res.status(200)
-      .json({ message: 'hello world' });
-});
 SocketController.start(io);
 
 console.log(process.env);
